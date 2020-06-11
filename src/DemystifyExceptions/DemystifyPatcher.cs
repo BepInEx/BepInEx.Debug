54f5211eb03e3ab6f9fe146f235f93994abc314a
ghorsington <neigh@coder.horse>
2020-06-11 19:03:52 +0300

    DemystifyExceptions: Port to HarmonyX

--- src/DemystifyExceptions/DemystifyPatcher.cs
+++ src/DemystifyExceptions/DemystifyPatcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using DemystifyExceptions.Demystify;
+using HarmonyLib;
 using Mono.Cecil;
 using MonoMod.RuntimeDetour;
 
@@ -10,45 +11,35 @@ namespace DemystifyExceptions
 {
     public static class DemystifyPatcher
     {
-        private static Detour exceptionToStringHook, exceptionStackTraceHook;
-
-        private static FieldInfo stackTrace;
-
         public static IEnumerable<string> TargetDLLs { get; } = new string[0];
 
         public static void Initialize()
         {
-            stackTrace = typeof(Exception).GetField("stack_trace", BindingFlags.Instance | BindingFlags.NonPublic);
-            if(stackTrace == null)
+            Harmony.CreateAndPatchAll(typeof(DemystifyPatcher), "org.bepinex.debug.demystifyexceptions");
+        }
+    
+        [HarmonyPatch(typeof(Exception), nameof(Exception.StackTrace), MethodType.Getter)]
+        [HarmonyPrefix]
+        public static bool ExceptionGetStackTrace(Exception __instance, ref string __result)
+        {
+            try
             {
-                Trace.TraceError("[DemystifyPatcher] Failed to find Exception.stack_trace field");
-                return;
+                var exStackTrace = new EnhancedStackTrace(__instance);
+                __result = exStackTrace.ToString();
+                return false;
             }
-
-            exceptionToStringHook = new Detour(typeof(Exception).GetMethod(nameof(Exception.ToString)),
-                typeof(DemystifyPatcher).GetMethod(nameof(ExceptionToStringHook)));
-            exceptionStackTraceHook =
-                new Detour(typeof(Exception).GetProperty(nameof(Exception.StackTrace)).GetGetMethod(),
-                    typeof(DemystifyPatcher).GetMethod(nameof(ExceptionGetStackTrace)));
-            exceptionToStringHook.Apply();
-            exceptionStackTraceHook.Apply();
+            catch (Exception)
+            {
+                return true;
+            }
         }
-
-        public static string ExceptionGetStackTrace(Exception self)
+        
+        [HarmonyPatch(typeof(Exception), nameof(Exception.ToString))]
+        [HarmonyPrefix]
+        public static bool ExceptionToStringHook(Exception __instance, ref string __result)
         {
-            if (stackTrace.GetValue(self) is string st)
-                return st;
-
-            var exStackTrace = new EnhancedStackTrace(self);
-            var stTemp = exStackTrace.ToString();
-
-            stackTrace.SetValue(self, stTemp);
-            return stTemp;
-        }
-
-        public static string ExceptionToStringHook(Exception self)
-        {
-            return self.ToStringDemystified();
+            __result = __instance.ToStringDemystified();
+            return true;
         }
 
         public static void Patch(AssemblyDefinition ad)
